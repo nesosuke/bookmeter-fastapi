0caf67325dbd54b34ca23cf0148e1a02a194d655
nesosuke <neso@neso.tech>
2021-03-01 15:40:32 +0900

    hello world

---
+++ app/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/hello")
+def hello():
+    return {"Hello": "World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
